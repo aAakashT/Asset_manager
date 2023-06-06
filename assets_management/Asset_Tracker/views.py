8ee35e06a2b8fb2e00d6b0f6bed73b88cc7202f8
aAkashT <aakashtorve66@gmail.com>
2023-06-06 12:39:38 +0530

    login_logout_functionality_added

--- assets_management/Asset_Tracker/views.py
+++ assets_management/Asset_Tracker/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
