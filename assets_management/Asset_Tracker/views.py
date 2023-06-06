7e2017a4d5cdabfd48960c8fa11c9b8dc8e0a06e
aAkashT <aakashtorve66@gmail.com>
2023-06-06 12:36:35 +0530

    login_logout_functionality_added

--- assets_management/Asset_Tracker/views.py
+++ assets_management/Asset_Tracker/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        remember_me = bool(request.POST.get('remember_me'))
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            if not remember_me:
+                request.session.set_expiry(0)  # Set session expiration to browser close
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid email or password')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
